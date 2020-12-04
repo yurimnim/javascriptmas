9bb1218278c883dcfbe7ff846111b2d583135c75
Yurim Lee <u007x2@gmail.com>
2020-12-04 10:52:03 +0900

    Dec 4th, 2020 Commit

--- Day3_chunky_monkeys/main.js
+++ Day3_chunky_monkeys/main.js
@@ -10,3 +10,37 @@ Example
 - chunkyMonky([0,1,2,3,4,5],4) should return [[0,1,2,3],[4,5]].
 
 */
+function chunkyMonkey(values, size) {
+  //  write code here.
+  let result = [];
+  valueSlice = values.slice(0, size);
+  valueSliceEnd = values.slice(size);
+
+  result.push(valueSlice);
+  result.push(valueSliceEnd);
+
+  return result;
+}
+
+/**
+ * Test Suite
+ */
+describe("chunkyMonkey()", () => {
+  it("returns largest positive integer possible for digit count", () => {
+    // arrange
+    const values = ["a", "b", "c", "d"];
+    const size = 2;
+
+    // act
+    const result = chunkyMonkey(values, size);
+
+    // log
+    console.log("result: ", result);
+
+    // assert
+    expect(result).toEqual([
+      ["a", "b"],
+      ["c", "d"]
+    ]);
+  });
+});
