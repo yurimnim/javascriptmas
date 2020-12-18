2f660997b4ea51e9c8d3deeca709bb530d65cad3
Yurim Lee <u007x2@gmail.com>
2020-12-18 19:02:07 +0900

    Dec 18th, 2020 Commit

--- Day17_different_symbols_naive/main.js
+++ Day17_different_symbols_naive/main.js
@@ -9,8 +9,38 @@ For s = 'cabca', the output should be 3.
 There are 3 different characters: a,b and c. 
 */
 
+//My Solution
 function differentSymbolsNaive(str) {
   //  write code here.
+  str = str.split('');
+  result = [];
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] !== str[i + 1] && !result.includes(str[i + 1])) {
+      result.push(str[i + 1]);
+    }
+  }
+  return result.length;
+}
+
+//Scrimba's solution (1)
+function differentSymbolsNaive2(str) {
+  const uniqueChars = [];
+  const chars = str.split('');
+
+  chars.forEach(char => {
+    if (!uniqueChars.includes(char)) {
+      uniqueChars.push(char);
+    }
+  });
+
+  return uniqueChars.length;
+}
+
+//Scrimba's solution (2)
+function differentSymbolsNaiveSet(str) {
+  const chars = str.split('');
+
+  return new Set(chars).size;
 }
 
 /**
