eb28a4047cc07f8f9d7e81d9d2cf43ad1976e640
Yurim Lee <u007x2@gmail.com>
2020-12-27 17:48:27 +0900

    Dec 27th, 2020 Commit

--- Day20_domain_type/main.js
+++ Day20_domain_type/main.js
@@ -14,12 +14,51 @@ labels the domains as "commercial", "organization", "network" or "information" f
 - for domains ["en.wiki.org" "codefights.com", "happy.net", "code.info"],
 the output should be 
 domainType(domains) = ["organization", "commercial", "network", "information"].
-
 */
 
 //My Solution
-function domainType(domains) {
-  //  write code here.
+const domainType = [];
+for (let i = 0; i < domains.length; i++) {
+  domainType.push(domains[i].split('.'));
+}
+
+const domainResult = [];
+for (let j = 0; j < domainType.length; j++) {
+  let domain = domainType[j][domainType[j].length - 1];
+
+  if (domain === 'org') {
+    domainResult.push('organization');
+  } else if (domain === 'com') {
+    domainResult.push('commercial');
+  } else if (domain === 'net') {
+    domainResult.push('network');
+  } else {
+    domainResult.push('information');
+  }
+
+  return domainResult;
+}
+
+//scrimba's solution
+function domainType2(domains) {
+  const domainTypes = [];
+
+  for (let i = 0; i < domains.length; i++) {
+    const urlPieces = domains[i].split('.');
+    const domain = urlPieces[urlPieces.length - 1];
+
+    if (domain === 'org') {
+      domainTypes.push('organization');
+    } else if (domain === 'com') {
+      domainTypes.push('commercial');
+    } else if (domain === 'net') {
+      domainTypes.push('network');
+    } else if (domain === 'info') {
+      domainTypes.push('information');
+    }
+  }
+
+  return domainTypes;
 }
 
 /**
