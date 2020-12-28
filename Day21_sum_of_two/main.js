5021603f4a75d490f81aeb0f4dc35de0dd029689
Yurim Lee <u007x2@gmail.com>
2020-12-28 14:55:43 +0900

    Dec 28th, 2020 Commit

--- Day21_sum_of_two/main.js
+++ Day21_sum_of_two/main.js
@@ -1,41 +1,52 @@
 /*
--- Day 22 --
-Extract Matrix Column
 
-Given a rectangular matrix and an integer column, return an array contraining the elements
-of the columnth column of the given matrix (the leftmost column is the 0th one)
+/*
+-- Day 21 --
+Sum of two
 
+You have two integer arrays, a and b, and an integer target value v. 
+Determine whether there is a pair of numbers, where one number is taken 
+from a and the other from b, that can be added together to get sum of v. 
+Return true if such a pair exists, otherwise return false. 
 
 -- Example --
-For matrix = [[1,1,1,2],[0,5,0,4],[2,1,3,6]] and column = 2, 
-the output should be extractMatrixColumn(matrix, column) = [1,0,3]
+For a = [1, 2, 3], b = [10, 20, 30, 40] , 
+and v = 42, the output should be sumOfTwo(a,b,v) = true
 */
 
-//My Solution
-function extractMatrixColumn(matrix, column) {
-  //  write code here.
+function sumOfTwo(nums1, nums2, value) {
+  const map = {};
+
+  for (let num of nums1) {
+    const difference = value - num;
+    map[difference] = difference; // {39: 39, 40: 40, 41: 41}
+  }
+
+  for (let num of nums2) {
+    if (map.hasOwnProperty(num)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
  * Test Suite
  */
-describe('extractMatrixColumn()', () => {
-  it('returns largest positive integer possible for digit count', () => {
+describe('sumOfTwo()', () => {
+  it('returns true if a value can be found that by adding one number from each list', () => {
     // arrange
-    const matrix = [
-      [1, 1, 1, 2],
-      [0, 5, 0, 4],
-      [2, 1, 3, 6],
-    ];
-    const column = 2;
+    const nums1 = [1, 2, 3];
+    const nums2 = [10, 20, 30, 40];
+    const value = 42;
 
     // act
-    const result = extractMatrixColumn(matrix, column);
+    const result = sumOfTwo(nums1, nums2, value);
 
     // log
     console.log('result: ', result);
 
     // assert
-    expect(result).toEqual([1, 0, 3]);
+    expect(result).toBe(true);
   });
 });
